7f5d8b88ca0e29b054a14c52d1d6d007e06b888c
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-05-08 22:08:40 -0700

    Update string.hpp

--- src/string.hpp
+++ src/string.hpp
@@ -3,28 +3,27 @@
 
 #include <compare>
 #include <iosfwd>
-
 #include "list.hpp"
 
 class String {
 public:
     // constructs this string from a C string, defaults to empty string
-    explicit String(const char *s = "");
+    explicit String(const char* s = "");
 
     // construct this string as a copy of string s
-    String(const String &s);
+    String(const String& s);
 
     // construct this string by moving from string s
-    String(String &&s);
+    String(String&& s);
 
     // swap this string with s
-    void swap(String &s);
+    void swap(String& s);
 
     // assignment operator from one string, s, to this string
-    String &operator=(const String &s);
+    String& operator=(const String& s);
 
     // assign to this string by moving
-    String &operator=(String &&s);
+    String& operator=(String&& s);
 
     // check if we can index at position i in this string
     bool in_bounds(int index) const;
@@ -32,7 +31,7 @@ public:
     // allow const indexing
     char operator[](int index) const;
 
-    // returns the logical length of this string (# of chars up to '\0')
+    // returns the logical length of this string
     int size() const;
 
     // returns a reversal of this string, does not modify this string
@@ -42,30 +41,31 @@ public:
     int indexOf(char c) const;
 
     // returns index into this string for first occurrence of s
-    int indexOf(const String &s) const;
+    int indexOf(const String& s) const;
 
     // compare this string with another string by equality
     // note: != is auto-generated since C++20
-    bool operator==(const String &s) const;
+    bool operator==(const String& s) const;
 
-    // C++20 way of defining all comparisons at once
-    std::strong_ordering operator<=>(const String &s) const;
+    // C++20 way of defining all comparisons at once, spaceship
+    std::strong_ordering operator<=>(const String& s) const;
 
     // concatenate this and s to form a return string
-    String operator+(const String &s) const;
+    String operator+(const String& s) const;
 
     // concatenate s onto the end of this string
-    String &operator+=(const String &s);
+    String& operator+=(const String& s);
 
     // print this string, hint: use operator << to send buf to out
-    void print(std::ostream &out) const;
+    void print(std::ostream& out) const;
 
     // read next word into this string
-    // hint: use operator >> to read from in into a temporary buffer
-    void read(std::istream &in);
+    // hint: use operator >> to read from in into buf
+    void read(std::istream& in);
 
     // destructor for this string
     ~String();
+
 private:
     list::Node* head;
 
@@ -73,7 +73,7 @@ private:
     explicit String(list::Node* head);
 };
 
-std::ostream &operator<<(std::ostream &out, const String &s);
-std::istream &operator>>(std::istream &in, String &s);
+std::ostream& operator<<(std::ostream& out, const String& s);
+std::istream& operator>>(std::istream& in, String& s);
 
 #endif
